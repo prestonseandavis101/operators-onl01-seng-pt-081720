d983f778738e78ee2bebf98199f8199c773a6d19
Preston Davis <prestonseandavis101@yahoo.com>
2020-08-24 03:15:30 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,15 +1,21 @@
 def unsafe?(speed)
   if speed > 60
     return true
-  
-end
+  elsif speed <40
+    return true
+  else speed == 50
+    return false
+
+
+  end
+  end
 
 
 def not_safe?(speed)
+  speed > 60 or speed < 40? true:false
   
-  
-	
-end
+
+  end
 	
 
 
